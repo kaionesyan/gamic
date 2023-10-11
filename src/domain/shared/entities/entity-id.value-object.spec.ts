853573cf9fc0366fd14e0fb43876874dd33600ad
Kaio <kaionesyan@gmail.com>
2023-10-10 21:06:12 -0300

    chore: finish entity id tests

--- src/domain/shared/entities/entity-id.value-object.spec.ts
+++ src/domain/shared/entities/entity-id.value-object.spec.ts
@@ -6,4 +6,38 @@ describe('Entity Id Value Object', () => {
 
     expect(entityId.value).toBeTruthy()
   })
+
+  it('should create a valid entity id from a string', () => {
+    const idString = 'c5f5fc8c-7276-4e7d-a8a3-e9ba9ff7b6db'
+
+    const entityId = EntityId.create(idString)
+
+    expect(entityId.value).toBe(idString)
+  })
+
+  it('should throw when creating an entity id from an invalid string', async () => {
+    const createInvalidEntityId = async (): Promise<EntityId> => {
+      return EntityId.create('invalid id')
+    }
+
+    await expect(createInvalidEntityId).rejects.toEqual(
+      new Error('Invalid UUID'),
+    )
+  })
+
+  it('should correctly compare identical entity ids', () => {
+    const idString = 'c5f5fc8c-7276-4e7d-a8a3-e9ba9ff7b6db'
+
+    const entityId1 = EntityId.create(idString)
+    const entityId2 = EntityId.create(idString)
+
+    expect(entityId1.equals(entityId2)).toBe(true)
+  })
+
+  it('should correctly compare unequal entity ids', () => {
+    const entityId1 = EntityId.create('c5f5fc8c-7276-4e7d-a8a3-e9ba9ff7b6db')
+    const entityId2 = EntityId.create('46877764-b5d3-41ab-9ff5-aae1951963b8')
+
+    expect(entityId1.equals(entityId2)).toBe(false)
+  })
 })
