95b0b1b1b2895e31ce8a4ea2e2df7afb8b546fd4
Kaio <kaionesyan@gmail.com>
2023-10-10 21:13:08 -0300

    chore: add base entity

---
+++ src/domain/shared/entities/base.entity.ts
@@ -0,0 +1,23 @@
+import { EntityId } from './entity-id.value-object'
+
+export abstract class BaseEntity<T> {
+  private _id: EntityId
+  protected props: T
+
+  get id(): EntityId {
+    return this._id
+  }
+
+  protected constructor(props: T, id?: string) {
+    this.props = props
+    this._id = id ? EntityId.create(id) : EntityId.create()
+  }
+
+  public equals(entity: BaseEntity<T>): boolean {
+    if (entity === this) return true
+
+    if (entity._id.equals(this._id)) return true
+
+    return false
+  }
+}
