135435f6fb0618d85e74426937651e799c4c3ef9
Kaio <kaionesyan@gmail.com>
2023-10-10 21:43:24 -0300

    chore: add users repository contract

---
+++ src/domain/users/repositories/users/users.repository.ts
@@ -0,0 +1,9 @@
+import { User } from '../../entities/user.entity'
+
+export abstract class UsersRepository {
+  abstract findByEmail(email: string): Promise<User | null>
+  abstract findByGamerTag(gamerTag: string): Promise<User | null>
+  abstract create(user: User): Promise<void>
+  abstract save(user: User): Promise<void>
+  abstract delete(user: User): Promise<void>
+}
