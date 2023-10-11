659f474ee70fd3ca99822a5d9026f2f25cca22cb
Kaio <kaionesyan@gmail.com>
2023-10-10 23:01:41 -0300

    chore: add prisma users repository

---
+++ src/infra/database/prisma/repositories/prisma-users.repository.ts
@@ -0,0 +1,62 @@
+import { UsersRepository } from '@/domain/users/repositories/users/users.repository'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { User } from '@/domain/users/entities/user.entity'
+import { GamerTag } from '@/domain/users/entities/gamer-tag.value-object'
+
+@Injectable()
+export class PrismaUsersRepository implements UsersRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.prismaService.user.findUnique({ where: { email } })
+
+    if (!user) return null
+
+    return User.create({
+      email: user.email,
+      passwordHash: user.passwordHash,
+      gamerTag: GamerTag.create(user.gamerTag),
+    })
+  }
+
+  async findByGamerTag(gamerTag: string): Promise<User | null> {
+    const user = await this.prismaService.user.findUnique({
+      where: { gamerTag },
+    })
+
+    if (!user) return null
+
+    return User.create({
+      email: user.email,
+      passwordHash: user.passwordHash,
+      gamerTag: GamerTag.create(user.gamerTag),
+    })
+  }
+
+  async create(user: User): Promise<void> {
+    await this.prismaService.user.create({
+      data: {
+        id: user.id.value,
+        email: user.email,
+        passwordHash: user.passwordHash,
+        gamerTag: user.gamerTag.value,
+      },
+    })
+  }
+
+  async save(user: User): Promise<void> {
+    await this.prismaService.user.update({
+      where: { id: user.id.value },
+      data: {
+        email: user.email,
+        passwordHash: user.passwordHash,
+        gamerTag: user.gamerTag.value,
+      },
+    })
+  }
+
+  async delete(user: User): Promise<void> {
+    await this.prismaService.user.delete({ where: { id: user.id.value } })
+  }
+}
--- src/infra/subdomains/users/users.module.ts
+++ src/infra/subdomains/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { RegisterUserController } from './controllers/register-user.controller'
+import { DatabaseModule } from '@/infra/database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [RegisterUserController],
 })
 export class UsersModule {}
