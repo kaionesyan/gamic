fd277bed2f75a66fa1c3a629497872593e5ae321
Kaio <kaionesyan@gmail.com>
2023-10-10 21:47:22 -0300

    chore: add register user use case

--- src/domain/shared/errors/domain.error.ts
+++ src/domain/shared/errors/domain.error.ts
@@ -1,7 +1,24 @@
-export enum DomainErrorCode {}
+export enum DomainErrorCode {
+  EmailInUse = 'EmailInUse',
+  GamerTagInUse = 'GamerTagInUse',
+}
 
 export class DomainError extends Error {
   public code: DomainErrorCode
+
+  static emailInUse = (email: string): DomainError => {
+    return new DomainError(
+      DomainErrorCode.EmailInUse,
+      `The email "${email}" is already in use`,
+    )
+  }
+
+  static gamerTagInUse = (gamerTag: string): DomainError => {
+    return new DomainError(
+      DomainErrorCode.GamerTagInUse,
+      `The gamer tag "${gamerTag}" is already in use`,
+    )
+  }
 
   protected constructor(code: DomainErrorCode, message: string) {
     super(message)
---
+++ src/domain/users/use-cases/register-user.use-case.ts
@@ -0,0 +1,45 @@
+import { DomainError } from '@/domain/shared/errors/domain.error'
+import { Result } from '@/domain/shared/types/result'
+import { User } from '../entities/user.entity'
+import { HashService } from '@/domain/shared/services/hash/hash.service'
+import { UsersRepository } from '../repositories/users/users.repository'
+import { GamerTag } from '../entities/gamer-tag.value-object'
+
+type Request = {
+  email: string
+  password: string
+  gamerTag: string
+}
+
+type Response = Result<DomainError, { user: User }>
+
+export class RegisterUserUseCase {
+  constructor(
+    private usersRepository: UsersRepository,
+    private hashService: HashService,
+  ) {}
+
+  async execute(data: Request): Promise<Response> {
+    const { email, password, gamerTag } = data
+
+    const emailInUse = await this.usersRepository.findByEmail(email)
+
+    if (emailInUse) {
+      return Result.err(DomainError.emailInUse(email))
+    }
+
+    const gamerTagInUse = await this.usersRepository.findByGamerTag(gamerTag)
+
+    if (gamerTagInUse) {
+      return Result.err(DomainError.gamerTagInUse(gamerTag))
+    }
+
+    const user = User.create({
+      email,
+      passwordHash: this.hashService.hash(password),
+      gamerTag: GamerTag.create(gamerTag),
+    })
+
+    return Result.ok({ user })
+  }
+}
